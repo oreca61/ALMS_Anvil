a4e787ee587bf38c656847f0a436bc108929d3e0
muhammet.altuntas@student.htl-rankweil.at <muhammet.altuntas@student.htl-rankweil.at>
2026-03-04 09:12:52 +0000

    Created ServerModule1 and Form1.RowTemplate1; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,3 +11,16 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+    return_value = anvil.server.call('query_database', 'SELECT name FROM gefaengnis')
+    return_value = [entry[0] for entry in return_value]
+    print(return_value)
+    self.drop_down_ALMS_auswahl.items = return_value
+    self.drop_down_ALMS_auswahl_change()
+
+  @handle("drop_down_ALMS_auswahl", "change")
+  def drop_down_ALMS_auswahl_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+    
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,34 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def query_database(query: str):
+  with sqlite3.connect(data_files["ALMS.db"]) as conn:
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return result
+
+@anvil.server.callable
+def query_database_dict(query: str):
+  with sqlite3.connect(data_files["ALMS.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
