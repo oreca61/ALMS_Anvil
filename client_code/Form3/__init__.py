fd7120d4f9763a853817a7c4f9c699a6a434fdb3
muhammet.altuntas@student.htl-rankweil.at <muhammet.altuntas@student.htl-rankweil.at>
2026-03-14 19:07:45 +0000

    Created Form3; Edited Form2

--- client_code/Form3/__init__.py
+++ client_code/Form3/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import Form3Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,14 +6,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form2(Form2Template):
+class Form3(Form3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  @handle("button_1", "click")
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Form1')
