e5313fe255b589f1c375967aa89a136505701aa5
muhammet.altuntas@student.htl-rankweil.at <muhammet.altuntas@student.htl-rankweil.at>
2026-03-14 20:15:49 +0000

    Edited ServerModule1 and Form3

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -36,30 +36,4 @@ def query_database_dict(query: str):
 
 @anvil.server.callable
 def hole_fahrer_plot_daten():
-  conn = sqlite3.connect("/mnt/data/ALMS.db")
-  cursor = conn.cursor()
-
-  query = """
-  SELECT 
-      f.fahrername,
-      r.strecke,
-      r.datum,
-      re.punkte
-  FROM rennergebnis re
-  JOIN fahrer f ON re.fahrer_id = f.fahrer_id
-  JOIN rennen r ON re.renn_id = r.renn_id
-  ORDER BY r.datum, f.fahrername
-  """
-
-  cursor.execute(query)
-  rows = cursor.fetchall()
-  conn.close()
-
-  daten = defaultdict(lambda: {"rennen": [], "punkte": []})
-
-  for fahrername, strecke, datum, punkte in rows:
-    rennen_label = f"{strecke} ({datum})"
-    daten[fahrername]["rennen"].append(rennen_label)
-    daten[fahrername]["punkte"].append(punkte)
-
-  return dict(daten)
+  return {"test": {"rennen": ["Rennen 1"], "punkte": [10]}}
