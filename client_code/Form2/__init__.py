f2b1fc419fd6c852b41118188563251fd1d33e6b
muhammet.altuntas@student.htl-rankweil.at <muhammet.altuntas@student.htl-rankweil.at>
2026-03-14 17:19:01 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -12,3 +12,8 @@ class Form2(Form2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("button_1", "click")
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Startseite.Gebaeude', self.item)
